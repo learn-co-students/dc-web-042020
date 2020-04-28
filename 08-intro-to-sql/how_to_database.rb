e9a02262b11b34c10ba69e9d9889b5daa4150ae8
Shannon Nabors <shannon.nabors@gmail.com>
2020-04-28 15:08:55 -0400

    adding lecture code

--- 08-intro-to-sql/how_to_database.rb
+++ 08-intro-to-sql/how_to_database.rb
@@ -4,5 +4,14 @@ require 'pry'
 
 db = SQLite3::Database.new('chinook.db')
 
-db.execute("select * from artists")
+puts db.execute("select * from artists")
 
+multi_line_sql = <<-SQL
+    SELECT DISTINCT artists.name FROM artists
+    JOIN albums ON albums.artist_id = artists.id
+    JOIN tracks ON tracks.album_id = albums.id
+    JOIN genres ON tracks.genre_id = genres.id
+    WHERE genres.name = "Pop";
+SQL
+
+puts db.execute(multi_line_sql)
