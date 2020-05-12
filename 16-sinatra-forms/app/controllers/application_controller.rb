52909565e31eb59465e368f289d5faab6cec6848
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-05-12 12:13:15 -0400

    post lecture

--- 16-sinatra-forms/app/controllers/application_controller.rb
+++ 16-sinatra-forms/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < Sinatra::Base
 
     set :views, "app/views" 
+    set :method_override, true
 
     get "/" do 
         erb :homepage
@@ -12,11 +13,52 @@ class ApplicationController < Sinatra::Base
         erb :index
     end
 
+    get "/books/new" do 
+        erb :new
+    end
+
+    post "/books" do 
+        title = params[:title]
+        author = params[:author]
+        snippet = params[:snippet]
+        book = Book.find_or_create_by(
+            title: title,
+            author: author,
+            snippet: snippet)
+        redirect "/books/#{book.id}"
+
+    end
+
     get "/books/:id" do
+        # get "/books/new"
         book_id = params[:id]
         @book = Book.find(book_id)
         erb :show
     end
+
+    get "/books/:id/edit" do 
+        @book = Book.find(params[:id])
+
+        erb :edit
+    end
+
+    put "/books/:id" do 
+        book = Book.find(params[:id])
+        book.update(title: params[:title],
+                     author: params[:author],
+                     snippet: params[:snippet])
+        redirect "/books/#{book.id}"
+    end
+
+    delete "/books/:id" do 
+        Book.delete(params[:id])
+        redirect "/books"
+    end
+    # get "/books/title/:title" do
+    #     book_title = params[:title]
+    #     @book = Book.find_by(title: book_title.capitalize)
+    #     erb :show
+    # end
 
     # get "/welcome" do 
     #     return "<h2>Welcome to our Book App</h2>"
