350d9bda89f1103a1b0151df35a81e8aed953055
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-05-18 12:03:10 -0400

    post lecture 20

---
+++ 20-rails-forms/snack-tracker/README.md
@@ -0,0 +1,5 @@
+### form_for vs form_tag
+
+form_tag: Generic, all-purpose form (e.g., login form)
+
+form_for: Use when the fields on a form correspond to the columns in a database (e.g., building a new object)
---
+++ 20-rails-forms/snack-tracker/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: "application#homepage"
+  get 'my_super_great_about_page', to: "application#about", as: "about"
+  # get 'snacks', to: "snacks#index"
+  # get 'snacks/:id', to: "snacks#show", as: "snack"
+  resources :snacks
+end
